6fb77b10998b6c4a4e61cfb6308872359da7a013
bhatia-di <bhatia.di@northeastern.edu>
2021-10-29 21:19:51 -0400

    Solved Challenge

--- src/CSVConsumerMain.java
+++ src/CSVConsumerMain.java
@@ -1,7 +1,6 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CSVConsumerMain {
 
@@ -9,20 +8,68 @@ public class CSVConsumerMain {
 
     public static void main(String[] args) {
         System.out.println("Begin Extraction");
-        File file = new File("./src");
-        for(String fileNames : file.list()) System.out.println(fileNames);
-//        List<List<String>> records = new ArrayList<>();
-//        try (BufferedReader br = new BufferedReader(new FileReader("D:\\Projects\\Vocera-Assesment\\.idea\\src\\DataInput.csv"))) {
-//            String line;
-//            while ((line = br.readLine()) != null) {
-//                String[] values = line.split(COMMA_DELIMITER);
-//                records.add(Arrays.asList(values));
-//            }
-//            System.out.println(records.size());
-//        } catch (FileNotFoundException fileNotFoundException) {
-//            fileNotFoundException.printStackTrace();
-//        } catch (IOException ioException) {
-//            ioException.printStackTrace();
-//        }
+        Map<String, Person> output = new HashMap<String, Person>();
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader("D:\\Projects\\Vocera-Assesment\\resources\\DataInput.csv"))) {
+            String row;
+            while ((row = br.readLine()) != null) {
+                String[] values = row.split(COMMA_DELIMITER);
+                String name = values[values.length-3].contains(":") ? null : values[values.length-3];
+                String packet = values[values.length-2].replaceAll("[\\D]", "");
+                Integer numberOfPackets = packet.isEmpty() ? null : Integer.parseInt(packet);
+                String sessionId = values[values.length-1];
+                Person per = Objects.isNull(output.get(sessionId)) ? new Person(): output.get(sessionId);
+
+                if (!Objects.isNull(name)) {
+                    per.setName(name);
+                }
+
+                if (!Objects.isNull(numberOfPackets)) {
+                    per.setNumberOfPackets(numberOfPackets);
+                }
+                output.put(sessionId, per);
+            }
+
+
+
+            // processing output
+            Map<String, Integer> personToPacketMapper = new HashMap<String, Integer>();
+            Person maxPerson = new Person();
+            maxPerson.setName(null);
+            maxPerson.setNumberOfPackets(0);
+
+            for (Map.Entry<String, Person> entry: output.entrySet()) {
+                int packetCount;
+                String packetDelivererName = entry.getValue().getName();
+                if (Objects.isNull(packetDelivererName)) continue;
+
+
+                else if (personToPacketMapper.containsKey(entry.getValue().getName())) {
+                    packetCount = entry.getValue().getNumberOfPackets() + personToPacketMapper.get(entry.getValue().getName());
+                    personToPacketMapper.put(packetDelivererName, packetCount );
+
+                } else {
+                    packetCount = entry.getValue().getNumberOfPackets();
+                    personToPacketMapper.put(packetDelivererName, packetCount);
+                }
+
+                if (maxPerson.getNumberOfPackets() < packetCount) {
+                    maxPerson.setNumberOfPackets(packetCount);
+                    maxPerson.setName(packetDelivererName);
+                }
+
+            }
+
+
+            System.out.println(maxPerson.getName() + " has dropped " + maxPerson.getNumberOfPackets() + " packets");
+
+
+
+
+
+        } catch (IOException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+
+    private String name;
+    private int numberOfPackets;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumberOfPackets(int numberOfPackets) {
+        this.numberOfPackets = numberOfPackets;
+    }
+
+    public int getNumberOfPackets() {
+        return numberOfPackets;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
